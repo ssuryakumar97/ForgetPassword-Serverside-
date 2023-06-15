9b522de940bec2e735fa9545c8e7e7d333aa9041
surya <ssuryakumar97@gmail.com>
2023-06-15 23:11:43 +0530

    link url changed

--- routes/auth.js
+++ routes/auth.js
@@ -80,7 +80,7 @@ router.post("/forget-password", async (req, res) => {
         const token = jwt.sign({email: oldUser.email, id: oldUser._id},secret, {
             expiresIn:"5m"
         });
-        const link = `http://localhost:4000/api/reset-password/${oldUser._id}/${token}`;
+        const link = `https://loginpage-reset.onrender.com/api/reset-password/${oldUser._id}/${token}`;
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
